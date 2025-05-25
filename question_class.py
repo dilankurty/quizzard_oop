edb8f74de04dab05214071e105c4ccd3a7aef861
Kurt <dilanmarinas@gmail.com>
2025-05-25 22:34:12 +0800

    added function to track whether the answer of user is correct

--- question_class.py
+++ question_class.py
@@ -5,4 +5,7 @@ class Question:
         self.answer = answer.upper()
         self.user_answer = None
 
+    def is_correct(self, user_choice):
+        self.user_answer = user_choice.upper()
+        return self.user_answer == self.answer
         
