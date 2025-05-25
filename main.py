023d903e03775ac12f7bb072b4ff264811e5a975
Kurt <dilanmarinas@gmail.com>
2025-05-26 01:29:22 +0800

    added clear terminal function to have a clean terminal when taking quiz

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from quiz_loader import QuizLoader
 from quiz_class import Quiz
 
 import os
+
+def clear_terminal():
+    """Clears the terminal screen."""
+    os.system("cls" if os.name == "nt" else "clear")
+
