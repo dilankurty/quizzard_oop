17812a5b579becace827924450d7c083040cf26b
Kurt <dilanmarinas@gmail.com>
2025-05-26 11:04:59 +0800

    added function under leaderboard class to save the score of the user in json format

--- leaderboard_class.py
+++ leaderboard_class.py
@@ -1,3 +1,16 @@
 import json
 import os
 from datetime import datetime
+
+class Leaderboard:
+    file_path = "leaderboard.json"
+
+    @staticmethod
+    def save_score(subject_name, user_name, score, total, percentage):
+        entry = {
+            "name": user_name,
+            "subject": subject_name,
+            "score": f"{score}/{total}",
+            "percentage": round(percentage, 2),
+            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
